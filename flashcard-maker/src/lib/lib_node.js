fdfdc01d7b07b09a79a55fa2d9098ad45eb569b5
MayKikuchi <m.may.kikuchi@gmail.com>
2020-05-18 16:57:36 -0700

    comment out file stuff in LibNode

--- flashcard-maker/src/lib/lib_node.js
+++ flashcard-maker/src/lib/lib_node.js
@@ -1,16 +1,17 @@
 class LibNode {
-    fs = require('fs');
-    writeDataFile(data) {
-        fs.writeFile('DataFile.txt', data, (err) => {
-            if (err) throw err;
-        })
-    }
-    readDataFile() {
-        fs.readFile('DataFile.txt', (err, data) => {
-            if (err) throw err;
-            return data;
-        })
-    }
+    // file stuff commented out for now: uncomment if needed
+    // fs = require('fs');
+    // writeDataFile(data) {
+    //     fs.writeFile('DataFile.txt', data, (err) => {
+    //         if (err) throw err;
+    //     })
+    // }
+    // readDataFile() {
+    //     fs.readFile('DataFile.txt', (err, data) => {
+    //         if (err) throw err;
+    //         return data;
+    //     })
+    // }
     tokenizer = null; // = getTokenizer(); // TODO: uncomment this after tokenizer is merged
 
     parse() {
