b3f1e12f930d60be6d2172cdd366f86dcc53b7d4
ZhiHanL <zhihanlim43@gmail.com>
2020-05-21 22:39:18 -0700

    Add Stats UI (#49)

--- src/components/Statistics.tsx
+++ src/components/Statistics.tsx
@@ -1,12 +1,69 @@
 import React from "react";
-// TODO
-// https://github.com/Dara2004/Goi/issues/49
+import StatisticsTable from "./StatisticsTable";
+import StatisticsOverview from "./StatisticsOverview";
+
+function getStatsObject() {
+  // temp return object for testing:
+  const rows = [
+    createStatsData(1, "Bonjour", "Hello", 1, 10, "French", ["test1", "test2"]),
+    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+  ];
+  return {
+    overview: {
+      // overview can have 0 or more of these key value pairs, depending on query
+      "average score": "5",
+      "highest score": "10",
+      "lowest score": "10",
+      "average time": "12min",
+      "total time": "80min",
+    },
+    details: rows,
+  };
+}
+
+function createStatsData(
+  index: number,
+  front: string,
+  back: string,
+  correct: number,
+  incorrect: number,
+  deck: string,
+  tags: string[]
+) {
+  const score = correct.toString() + "/" + incorrect.toString();
+  const tagsString = tags.join(", ");
+  const indexString = index.toString() + ".)";
+  return { indexString, front, back, score, deck, tagsString };
+}
 
 export default function Statistics() {
+  const statsObject = getStatsObject();
   return (
     <>
-      <div className="statistics-header">
+      <div className="card-view">
         <h1> Statistics</h1>
+        <StatisticsOverview overview={statsObject.overview} />
+        <StatisticsTable rows={statsObject.details} />
       </div>
     </>
   );
