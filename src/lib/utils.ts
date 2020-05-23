aad178c79d1e7227a4b025f9931d477ceeba0eaa
Kaitian Xie <xkaitian@gmail.com>
2020-05-22 17:03:48 -0700

    Add debug

---
+++ src/lib/utils.ts
@@ -0,0 +1,9 @@
+export function debug(...items: any): void {
+  if (isDebugEnabled()) {
+    console.log(...items);
+  }
+}
+
+function isDebugEnabled(): boolean {
+  return process.env.REACT_APP_DEBUG.toLowerCase() === "on";
+}
