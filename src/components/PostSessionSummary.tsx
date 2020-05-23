c4459470d7ad58926d5eb200cc5371d5919d82ac
MayKikuchi <48613270+MayKikuchi@users.noreply.github.com>
2020-05-22 22:56:21 -0700

    Ui summary (#74)
    * add summary screen

--- src/components/PostSessionSummary.tsx
+++ src/components/PostSessionSummary.tsx
@@ -1,10 +1,138 @@
 import React from "react";
-// TODO
-// https://github.com/Dara2004/Goi/issues/48
+import { createCardData } from "../lib/util";
+import StatisticsTable from "./StatisticsTable";
+import ConfettiEmoji from "../assets/confettiEmoji.png";
+
+function getSessionCards() {
+  // temp return object for testing:
+
+  // same type of return object as for Stats - assuming the session data will be
+  // saved upon session completion, and will be retrieved using the same query as
+  // for stats, but only for the current/most recent session
+  // because this is only one session, 'correct' and 'incorrect' should only ever be 0 or 1
+  return {
+    overview: { "total time": "80min" },
+    details: [
+      createCardData(
+        2,
+        "Aurevoir",
+        "Bye",
+        1,
+        0,
+        "French",
+        ["test1", "test2"],
+        true
+      ),
+      createCardData(
+        3,
+        "Aurevoir",
+        "Bye",
+        1,
+        0,
+        "French",
+        ["test1", "test2"],
+        true
+      ),
+      createCardData(
+        4,
+        "Aurevoir",
+        "Bye",
+        1,
+        0,
+        "French",
+        ["test1", "test2"],
+        true
+      ),
+      createCardData(
+        5,
+        "Aurevoir",
+        "Bye",
+        1,
+        1,
+        "French",
+        ["test1", "test2"],
+        true
+      ),
+      createCardData(
+        6,
+        "Aurevoir",
+        "Bye",
+        1,
+        0,
+        "French",
+        ["test1", "test2"],
+        true
+      ),
+      createCardData(
+        7,
+        "Aurevoir",
+        "Bye",
+        1,
+        1,
+        "French",
+        ["test1", "test2"],
+        true
+      ),
+      createCardData(
+        8,
+        "Aurevoir",
+        "Bye",
+        1,
+        1,
+        "French",
+        ["test1", "test2"],
+        true
+      ),
+      createCardData(
+        9,
+        "Aurevoir",
+        "Bye",
+        1,
+        0,
+        "French",
+        ["test1", "test2"],
+        true
+      ),
+    ],
+  };
+}
+
+function getSessionScore(cardArray: any[]): [string, boolean] {
+  const reducer = (accumulator, currentValue) => {
+    const cardNumerator =
+      currentValue && currentValue.score === "correct" ? 1 : 0;
+    return accumulator + cardNumerator;
+  };
+  const numerator = cardArray.reduce(reducer, 0);
+  const denominator = cardArray.length;
+
+  const numeratorString = numerator && numerator.toString();
+  const denominatorString = denominator && denominator.toString();
+
+  const sessionScore = [numeratorString, denominatorString].join("/");
+  const isAllCorrect = numerator === denominator;
+
+  return [sessionScore, isAllCorrect];
+}
+
 export default function PostSessionSummary() {
+  const sessionCards = getSessionCards();
+  const [score, isAllCorrect]: [string, boolean] = getSessionScore(
+    sessionCards.details
+  );
   return (
-    <>
+    <div className="right-side-container">
       <h1>Summary</h1>
-    </>
+      <br />
+      <h2>You got: {score} cards correct</h2>
+      {isAllCorrect && <h2>Good job!!</h2>}
+      {isAllCorrect && (
+        <img className="confetti" src={ConfettiEmoji} alt={"Confetti"} />
+      )}
+      <br />
+      <h3>Time spent: {sessionCards.overview["total time"]}</h3>
+      <br />
+      <StatisticsTable rows={sessionCards.details} isForSummary={true} />
+    </div>
   );
 }
--- src/components/Statistics.tsx
+++ src/components/Statistics.tsx
@@ -1,33 +1,10 @@
 import React from "react";
 import StatisticsTable from "./StatisticsTable";
 import StatisticsOverview from "./StatisticsOverview";
+import { createCardData } from "../lib/util";
 
 function getStatsObject() {
   // temp return object for testing:
-  const rows = [
-    createStatsData(1, "Bonjour", "Hello", 1, 10, "French", ["test1", "test2"]),
-    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
-    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
-    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
-    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
-    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
-    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
-    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
-    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
-    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
-    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
-    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
-    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
-    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
-    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
-    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
-    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
-    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
-    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
-    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
-    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
-    createStatsData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
-  ];
   return {
     overview: {
       // overview can have 0 or more of these key value pairs, depending on query
@@ -37,23 +14,33 @@ function getStatsObject() {
       "average time": "12min",
       "total time": "80min",
     },
-    details: rows,
+    details: [
+      createCardData(1, "Bonjour", "Hello", 1, 10, "French", [
+        "test1",
+        "test2",
+      ]),
+      createCardData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+      createCardData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+      createCardData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+      createCardData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+      createCardData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+      createCardData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+      createCardData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+      createCardData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+      createCardData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+      createCardData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+      createCardData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+      createCardData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+      createCardData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+      createCardData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+      createCardData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+      createCardData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+      createCardData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+      createCardData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+      createCardData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+      createCardData(2, "Aurevoir", "Bye", 5, 10, "French", ["test1", "test2"]),
+    ],
   };
-}
-
-function createStatsData(
-  index: number,
-  front: string,
-  back: string,
-  correct: number,
-  incorrect: number,
-  deck: string,
-  tags: string[]
-) {
-  const score = correct.toString() + "/" + incorrect.toString();
-  const tagsString = tags.join(", ");
-  const indexString = index.toString() + ".)";
-  return { indexString, front, back, score, deck, tagsString };
 }
 
 export default function Statistics() {
@@ -63,7 +50,7 @@ export default function Statistics() {
       <div className="right-side-container">
         <h1> Statistics</h1>
         <StatisticsOverview overview={statsObject.overview} />
-        <StatisticsTable rows={statsObject.details} />
+        <StatisticsTable rows={statsObject.details} isForSummary={false} />
       </div>
     </>
   );
