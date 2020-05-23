f9c6db1bea2f3d62e6c7243a7c314a4f5e611363
MayKikuchi <m.may.kikuchi@gmail.com>
2020-05-23 00:47:27 -0700

    create EXPORT_DECK and LOAD_DECK nodes

--- src/ast/NODE.js
+++ src/ast/NODE.js
@@ -1,19 +1,7 @@
 import Tokenizer from "../lib/tokenizer";
 
 export default class NODE {
-  fs = require("fs");
-  writeDataFile(data) {
-    this.fs.writeFile("DataFile.txt", data, (err) => {
-      if (err) throw err;
-    });
-  }
-  readDataFile() {
-    this.fs.readFile("DataFile.txt", (err, data) => {
-      if (err) throw err;
-      return data;
-    });
-  }
-  tokenizer = Tokenizer.getTokenizer(); // = getTokenizer(); // TODO: uncomment this after tokenizer is merged
+  tokenizer = Tokenizer.getTokenizer();
   parse() {
     throw new Error("parse not implemented");
   }
