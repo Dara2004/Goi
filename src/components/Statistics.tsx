56cf7ffcbefcf4836faa04c453b2ea9b47074222
Zhi Han <32226443+ZhiHanL@users.noreply.github.com>
2020-05-24 10:59:15 -0700

    Add different column types based on request stat (#81)

--- src/components/Statistics.tsx
+++ src/components/Statistics.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import StatisticsTable from "./StatisticsTable";
 import StatisticsOverview from "./StatisticsOverview";
+import { ColumnType } from "./StatisticsTable";
 import { createCardData } from "../lib/util";
 
 function getStatsObject() {
   // temp return object for testing:
   return {
     overview: {
-      // overview can have 0 or more of these key value pairs, depending on query
       "average score": "5",
       "highest score": "10",
       "lowest score": "10",
@@ -50,7 +50,10 @@ export default function Statistics() {
       <div className="right-side-container">
         <h1> Statistics</h1>
         <StatisticsOverview overview={statsObject.overview} />
-        <StatisticsTable rows={statsObject.details} isForSummary={false} />
+        <StatisticsTable
+          rows={statsObject.details}
+          columnType={ColumnType.CARD_COLUMNS}
+        />
       </div>
     </>
   );
