ee9f02ac618ac1b84a34ae9809a7c7dc48acb108
MayKikuchi <m.may.kikuchi@gmail.com>
2020-05-24 12:18:00 -0700

    del valid

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -10,16 +10,8 @@ export const keywords = [
   "cards from",
 ];
 export const validCardFilter = ["best", "worst", "random", "oldest", "newest"];
-export const validStat = ["minimum", "maximum", "average"];
-export const validStatItem = ["time spent on", "scores for"];
 export const subjectKeywords = ["Decks:", "Tags:", "Sessions"];
 
-export const allTokens = [
-  ...validCardFilter,
-  ...validStat,
-  ...validStatItem,
-  ...keywords,
-  ...subjectKeywords,
-];
+export const allTokens = [...validCardFilter, ...keywords, ...subjectKeywords];
 
 export const deckCreationLiterals = ["Create Deck", ":", "\\(", "\\)", ","];
