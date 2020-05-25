c05a5965eccabff055e0890e9b1a49d237b8fe37
MayKikuchi <48613270+MayKikuchi@users.noreply.github.com>
2020-05-24 19:52:25 -0700

    fix export load bugs (#89)

--- src/ast/LOAD_DECKS.ts
+++ src/ast/LOAD_DECKS.ts
@@ -1,10 +1,7 @@
 import NODE from "./NODE";
 
 export default class LOAD_DECKS extends NODE {
-  isLoadDecks: boolean;
-
   parse() {
     this.tokenizer.getAndCheckToken("Load decks");
-    this.isLoadDecks = true;
   }
 }
